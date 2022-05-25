5bfd326e621636722f876a0c0327b5911a3f73bc
Lap Van Pham <caramel-apple@outlook.com>
2022-05-25 10:56:20 +0700

    default

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Toolbar from "./toolbar"
+
+export default function About() {
+
+
+  return (
+    <div>
+      <Toolbar />
+      <div>This is about page</div>
+    </div>
+  )
+}
---
+++ pages/blog.js
@@ -0,0 +1,10 @@
+import Toolbar from "./toolbar";
+
+export default function Blog() {
+  return (
+    <div>
+      <Toolbar />
+      <div>I am blog page!</div>
+    </div>
+  )
+}
